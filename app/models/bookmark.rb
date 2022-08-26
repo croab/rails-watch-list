4dc9845debc528404541b1dc29e14d22170c2b1b
Benjamin Croad <benjamincroadapple@gmail.com>
2022-08-26 11:52:00 +0100

    add dotenv-rails

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
-  belongs_to :movie, dependent: :destroy
+  belongs_to :movie
+  #
   validates :comment, length: { minimum: 6, too_short: "%{count} characters is the minimum allowed" }
   validates :movie_id, uniqueness: { scope: :list_id, message: "bookmark should be unique" }
 end
